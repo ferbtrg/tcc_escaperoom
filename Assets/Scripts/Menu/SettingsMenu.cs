a473d70711eac881f9317be2779388510fd8a3a2
Fernanda Buitrago <140107700+ferbtrg@users.noreply.github.com>
2024-11-23 02:36:27 -0300

    Configurações de setting

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,11 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public void SetVolume(float volume)
+    public AudioMixer               _audioMixer;
+    Resolution[]                    _resolutions;
+    public TMPro.TMP_Dropdown       _resolutionDropDown;
+
+    private void Start()
     {
+        _resolutions = Screen.resolutions;
+
+        _resolutionDropDown.ClearOptions();
+
+        var options = new List<string>(); 
+        int currentResolutionIndex = 0;
+        for( int i = 0; i < _resolutions.Length; i++)
+        {
+            string option =  _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add( option );
+
+            if( _resolutions[i].width == Screen.currentResolution.width && 
+                _resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        _resolutionDropDown.AddOptions(options);
+        _resolutionDropDown.value = currentResolutionIndex;
+        _resolutionDropDown.RefreshShownValue();
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        var resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution( resolution.width, resolution.height, Screen.fullScreen );
+    }
+
+    public void SetVolume( float volume )
+    {
+        _audioMixer.SetFloat( "volume", volume );
+    
         Debug.Log(volume);
+    }
+
+    public void SetQuality( int qualityIndex )
+    {
+        QualitySettings.SetQualityLevel( qualityIndex );
     }
 }
